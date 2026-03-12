fde63bcf824f654ed99717a95211d7ee00793b66
vinikataria <87491380+vinikataria@users.noreply.github.com>
2026-03-12 21:54:17 +0530

    Add files via upload

--- WebApi2/Models/Employee.cs
+++ WebApi2/Models/Employee.cs
@@ -1,1 +1,11 @@
+﻿namespace WebApi2.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string? Name { get; set; }
 
+        public string? Password { get; set; }
+
+    }
+}
---
+++ WebApi2/Models/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApi2.Models
+{
+    public class Response
+    {
+        public int StatusCode { get; set; }
+        public string? ErrorMsg { get; set; }
+    }
+}
